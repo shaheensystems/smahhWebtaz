69f5b8d72bfd254348753447e2497976d15081da
Taz <tasawerk@gmail.com>
2025-01-16 22:47:56 +0500

    removed a apostrophe from resource details

--- app/resources/[slug]/ResourceDetails.tsx
+++ app/resources/[slug]/ResourceDetails.tsx
@@ -6,7 +6,7 @@ const ResourceDetails = () => {
       <h2 className='text-3xl font-semibold text-center pb-6'>
         Discover Comprehensive Resources for Cybersecurity Solutions <br /> Tailored to Your Needs
       </h2>
-      <p className='text-center'>Explore expert insights, tools, and strategies designed to strengthen your organisation's security,<br /> 
+      <p className='text-center'>Explore expert insights, tools, and strategies designed to strengthen your organisations security,<br /> 
         ensuring resilience against evolving cyber threats.</p>
         <p className='text-center'>Gain access to detailed guides and resources, empowering your team to identify and mitigate risks<br />  with confidence and efficiency.</p>
     </div>
